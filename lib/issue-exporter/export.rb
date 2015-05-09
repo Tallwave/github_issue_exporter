80c4946ee52ab8be686347ad7eda9ef8a7aee996
Scott Williams <scott@swilliams.me>
2015-05-09 15:13:23 -0700

    make exporter into class

--- lib/issue-exporter/export.rb
+++ lib/issue-exporter/export.rb
@@ -3,11 +3,34 @@
 require 'net/http'
 require 'json'
 
-def make_url(owner, repo, token)
-  url_format = "https://api.github.com/repos/%s/%s/issues?access_token=%s"
-  url_format % [owner, repo, token]
-end
+module IssueExporting
+  class Exporter
 
+    def initialize(owner, repo, token)
+      @owner = owner
+      @repo = repo
+      @token = token
+    end
+
+    def export
+      url = URI.parse make_url
+      response = Net::HTTP::get url
+      write_to_file response
+    end
+
+    private
+    def make_url
+      url_format = "https://api.github.com/repos/%s/%s/issues?access_token=%s"
+      url_format % [@owner, @repo, @token]
+    end
+
+    def write_to_file(response_text)
+      filename = "issues.json"
+      File.open(filename, 'w') { |f| f.write response_text }
+    end
+
+  end
+end
 
 
 #url = URI.parse ""
