02d32495f9d297f9223e6f0e3607f4b4a5de1c12
Scott Williams <scott@swilliams.me>
2015-05-11 19:59:05 -0700

    init importer

--- lib/issue_exporter/import.rb
+++ lib/issue_exporter/import.rb
@@ -3,12 +3,19 @@ require 'json'
 
 module IssueExporting
   class Importer
+    def initialize(files)
+      @files = files
+    end
+
+    def import
+    end
+
+    private
     def read_file(filename)
     end
 
     def create_issue
     end
 
-    private
   end
 end
