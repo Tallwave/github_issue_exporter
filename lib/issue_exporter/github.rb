bbbfededcc0908971fb0c300a22e62336c2a7e56
Scott Williams <scott@swilliams.me>
2015-07-21 20:31:30 -0700

    Add closed flag to URL builder

--- lib/issue_exporter/github.rb
+++ lib/issue_exporter/github.rb
@@ -5,13 +5,15 @@ module IssueExporting
     "https://api.github.com/repos/%s/%s/issues?access_token=%s"
   end
 
-  def self.make_url(owner, repo, token)
+  def self.make_url(owner, repo, token, options = {})
     url_format = IssueExporting.api_url
-    url_format % [owner, repo, token]
+    root_url = url_format % [owner, repo, token]
+    return root_url if options[:include_closed_issues].nil?
+    root_url + "&state=all"
   end
 
-  def self.make_uri(owner, repo, token)
-    URI.parse IssueExporting.make_url(owner, repo, token)
+  def self.make_uri(owner, repo, token, options = {})
+    URI.parse IssueExporting.make_url(owner, repo, token, options)
   end
 
   def self.turn_options_into_querystring(options)
